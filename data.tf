0b7c02c2862075c1af50be3ffdf1bbe797c9483d
priyankapeelu <priyankapeelu@icloud.com>
2022-06-16 18:16:08 +0530

    terraform variablescd tt

--- data.tf
+++ data.tf
@@ -1,11 +1,11 @@
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex  = "base-with-ansible"
+  name_regex  = "base_with_ansible"
   owners      = ["self"]
 }
 
 data "aws_secretsmanager_secret" "secrets" {
-  name = "dev/roboshop/secrets"
+  name = "dev/roboshop/secret"
 }
 
 data "aws_secretsmanager_secret_version" "secrets" {
